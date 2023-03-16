5bbc2dd26f89687c652a003cb5b1d3afafbabcd6
lujiangyong <mulang123@outlook.com>
2023-03-16 10:54:19 +0800

    Initial commit

---
+++ app/src/main/java/com/mvgx/campmobile/AppApplication.java
@@ -0,0 +1,22 @@
+package com.mvgx.campmobile;
+
+import com.mvgx.common.config.ModuleLifecycleConfig;
+
+import me.goldze.mvvmhabit.base.BaseApplication;
+
+/**
+ * @Author Arthur
+ * @Date 2023/3/14 14:53
+ */
+public class AppApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化组件(靠前)
+        ModuleLifecycleConfig.getInstance().initModuleAhead(this);
+        //....
+        //初始化组件(靠后)
+        ModuleLifecycleConfig.getInstance().initModuleLow(this);
+    }
+}
+
